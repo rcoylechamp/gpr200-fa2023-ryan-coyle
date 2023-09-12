601172e3ec8cf570af8af281e47f967e3cfd4fbf
Ryan Coyle <ryan.coyle@mymail.champlain.edu>
2023-09-11 21:22:28 -0400

    had to redo because repository error. Finished refactoring shaders

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -8,6 +8,55 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+float vertices[9] = {
+	//x   //y  //z
+	-0.5, -0.5, 0.0, //Bottom left
+	 0.5, -0.5, 0.0, //Bottom right
+	 0.0,  0.5, 0.0  //Top center
+};
+
+//main.cpp
+const char* vertexShaderSource = R"(
+#version 450
+layout(location = 0) in vec3 vPos;
+void main(){
+gl_Position = vec4(vPos,1.0);
+}
+)";
+
+//main.cpp
+const char* fragmentShaderSource = R"(
+#version 450
+out vec4 FragColor;
+void main(){
+	FragColor = vec4(1.0,0.0,0.0,1.0);
+}
+)";
+
+
+
+unsigned int createShader(GLenum shaderType, const char* sourceCode) {
+	
+	
+
+	//Supply the shader object with source code
+	glShaderSource(shaderType, 1, &sourceCode, NULL);
+	//Compile the shader object
+	glCompileShader(shaderType);
+
+	int success;
+	glGetShaderiv(shaderType, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		//512 is an arbitrary length, but should be plenty of characters for our error message.
+		char infoLog[512];
+		glGetShaderInfoLog(shaderType, 512, NULL, infoLog);
+		printf("Failed to compile shader: %s", infoLog);
+
+	}
+
+	return success;
+}
+
 int main() {
 	printf("Initializing...");
 	if (!glfwInit()) {
@@ -26,6 +75,56 @@ int main() {
 		printf("GLAD Failed to load GL headers");
 		return 1;
 	}
+
+
+
+	//Define a new buffer id
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	//Allocate space for + send vertex data to GPU.
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	//Tell vao to pull vertex data from vbo
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	//Define position attribute (3 floats)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glEnableVertexAttribArray(0);
+
+
+	
+	unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
+	unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+
+	
+
+	unsigned int shaderProgram = glCreateProgram();
+	//Attach each stage
+	glAttachShader(shaderProgram, vertexShader);
+	//glAttachShader(shaderProgram, geometryShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	//Link all the stages together
+	glLinkProgram(shaderProgram);
+
+
+	//glLinkProgram...
+//Check for linking errors
+	int success;
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		char infoLog[512];
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		printf("Failed to link shader program: %s", infoLog);
+	}
+	//The linked program now contains our compiled code, so we can delete these intermediate objects
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
