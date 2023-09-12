09621152f5f771c6d05ba2a346ed3a9664fa23f3
Ryan Coyle <ryan.coyle@mymail.champlain.edu>
2023-09-11 22:50:12 -0400

    got color in triangle, but undid refactoring because it didn't work

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -8,33 +8,36 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
-float vertices[9] = {
-	//x   //y  //z
-	-0.5, -0.5, 0.0, //Bottom left
-	 0.5, -0.5, 0.0, //Bottom right
-	 0.0,  0.5, 0.0  //Top center
+float vertices[21] = {
+	//x   //y  //z   //r  //g  //b  //a
+	-0.5, -0.5, 0.0, 1.0, 0.0, 0.0, 1.0, //Bottom left
+	 0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, //Bottom right
+	 0.0,  0.5, 0.0, 0.0, 0.0, 1.0, 1.0  //Top center
 };
 
-//main.cpp
+
 const char* vertexShaderSource = R"(
-#version 450
-layout(location = 0) in vec3 vPos;
-void main(){
-gl_Position = vec4(vPos,1.0);
-}
+	#version 450
+	layout(location = 0) in vec3 vPos;
+	layout(location = 1) in vec4 vColor;
+	out vec4 Color;
+	void main(){
+		Color = vColor;
+		gl_Position = vec4(vPos,1.0);
+	}
 )";
 
-//main.cpp
 const char* fragmentShaderSource = R"(
-#version 450
-out vec4 FragColor;
-void main(){
-	FragColor = vec4(1.0,0.0,0.0,1.0);
-}
+	#version 450
+	out vec4 FragColor;
+	in vec4 Color;
+	void main(){
+		FragColor = Color;
+	}
 )";
 
 
-
+/*
 unsigned int createShader(GLenum shaderType, const char* sourceCode) {
 	
 	
@@ -42,10 +45,10 @@ unsigned int createShader(GLenum shaderType, const char* sourceCode) {
 	//Supply the shader object with source code
 	glShaderSource(shaderType, 1, &sourceCode, NULL);
 	//Compile the shader object
-	glCompileShader(shaderType);
+	glCompileShader(shaderType); 
 
 	int success;
-	glGetShaderiv(shaderType, GL_COMPILE_STATUS, &success);
+	 glGetShaderiv(shaderType, GL_COMPILE_STATUS, &success);
 	if (!success) {
 		//512 is an arbitrary length, but should be plenty of characters for our error message.
 		char infoLog[512];
@@ -54,8 +57,9 @@ unsigned int createShader(GLenum shaderType, const char* sourceCode) {
 
 	}
 
-	return success;
+	return shaderType;
 }
+*/
 
 int main() {
 	printf("Initializing...");
@@ -92,15 +96,31 @@ int main() {
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 
 	//Define position attribute (3 floats)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (const void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)0);
 	glEnableVertexAttribArray(0);
 
+	//Color attribute
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (const void*)(sizeof(float) * 3));
+	glEnableVertexAttribArray(1);
+	
+	//unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
+	//unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
 
 	
-	unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
-	unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	//Supply the shader object with source code
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	//Compile the shader object
+	glCompileShader(vertexShader);
 
+	//Create a new vertex shader object
+	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	//Supply the shader object with source code
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	//Compile the shader object
+	glCompileShader(fragmentShader);
 	
+
 
 	unsigned int shaderProgram = glCreateProgram();
 	//Attach each stage
@@ -130,6 +150,10 @@ int main() {
 		glfwPollEvents();
 		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		glfwSwapBuffers(window);
 	}
 	printf("Shutting down...");
