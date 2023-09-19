b2ea8da4315cd1b0e156e4dee7153a59c9c768c3
Ryan Coyle <ryan.coyle@mymail.champlain.edu>
2023-09-19 11:13:38 -0400

    moved shader functions from main to rc .h and .cpp files. Need to finish implementing vec2, vec3, and vec4

--- assignments/assignment2_sunset/main.cpp
+++ assignments/assignment2_sunset/main.cpp
@@ -12,8 +12,8 @@
 //can use namespace like below, but easier to see if using "rc::"
 //using namespace rc; 
 
-unsigned int createShader(GLenum shaderType, const char* sourceCode);
-unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource);
+
+
 unsigned int createVAO(float* vertexData, int numVertices);
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 
@@ -67,13 +67,18 @@ int main() {
 
 	std::string vertexShaderSource = rc::loadShaderSourceFromFile("assets/vertexShader.vert");
 	std::string fragmentShaderSource = rc::loadShaderSourceFromFile("assets/fragmentShader.frag");
-	unsigned int shader = createShaderProgram(vertexShaderSource.c_str(), fragmentShaderSource.c_str());
+	
 
+	rc::Shader shader("assets/vertexShader.vert", "assets/fragmentShader.frag");
+	shader.use();
+	shader.setFloat("_MyFloat", triangleBrightness);
+	shader.setVec2("_MyVec2", vec2[0], vec2[1]);
 
 	unsigned int vao = createVAO(vertices, 3);
 
-	glUseProgram(shader);
+	//glUseProgram(shader);
 	glBindVertexArray(vao);
+
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -81,8 +86,8 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		//Set uniforms
-		glUniform3f(glGetUniformLocation(shader, "_Color"), triangleColor[0], triangleColor[1], triangleColor[2]);
-		glUniform1f(glGetUniformLocation(shader,"_Brightness"), triangleBrightness);
+		//glUniform3f(glGetUniformLocation(shader, "_Color"), triangleColor[0], triangleColor[1], triangleColor[2]);
+		//glUniform1f(glGetUniformLocation(shader,"_Brightness"), triangleBrightness);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
@@ -111,46 +116,7 @@ int main() {
 }
 
 
-unsigned int createShader(GLenum shaderType, const char* sourceCode) {
-	//Create a new vertex shader object
-	unsigned int shader = glCreateShader(shaderType);
-	//Supply the shader object with source code
-	glShaderSource(shader, 1, &sourceCode, NULL);
-	//Compile the shader object
-	glCompileShader(shader);
-	int success;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-	if (!success) {
-		//512 is an arbitrary length, but should be plenty of characters for our error message.
-		char infoLog[512];
-		glGetShaderInfoLog(shader, 512, NULL, infoLog);
-		printf("Failed to compile shader: %s", infoLog);
-	}
-	return shader;
-}
 
-unsigned int createShaderProgram(const char* vertexShaderSource, const char* fragmentShaderSource) {
-	unsigned int vertexShader = createShader(GL_VERTEX_SHADER, vertexShaderSource);
-	unsigned int fragmentShader = createShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
-
-	unsigned int shaderProgram = glCreateProgram();
-	//Attach each stage
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	//Link all the stages together
-	glLinkProgram(shaderProgram);
-	int success;
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success) {
-		char infoLog[512];
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		printf("Failed to link shader program: %s", infoLog);
-	}
-	//The linked program now contains our compiled code, so we can delete these intermediate objects
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-	return shaderProgram;
-}
 
 unsigned int createVAO(float* vertexData, int numVertices) {
 	unsigned int vao;
