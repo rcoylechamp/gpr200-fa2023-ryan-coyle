c1f4d90dce674c061addf47d8daf17ab1ad5a0e1
Ryan Coyle <ryan.coyle@mymail.champlain.edu>
2023-11-28 11:11:59 -0500

    Final Project folder

---
+++ assignments/finalProject/main.cpp
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+#include <ew/ewMath/ewMath.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <rc/shader.h>
+#include <ew/shader.h>
+#include <rc/texture.h>
+
+struct Vertex {
+	float x, y, z;
+	float u, v;
+};
+
+unsigned int createVAO(Vertex* vertexData, int numVertices, unsigned short* indicesData, int numIndices);
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+const int SCREEN_WIDTH = 1080;
+const int SCREEN_HEIGHT = 720;
+
+Vertex vertices[4] = {
+	{-1.0, -1.0, 0.0, 0.0, 0.0},
+	{1.0, -1.0, 0.0, 1.0, 0.0},
+	{1.0, 1.0, 0.0, 1.0, 1.0},
+	{-1.0, 1.0, 0.0, 0.0, 1.0}
+};
+unsigned short indices[6] = {
+	0, 1, 2,
+	2, 3, 0
+};
+
+int main() {
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return 1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Textures", NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return 1;
+	}
+
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	//Initialize ImGUI
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	//ew::Shader backgroundShader("assets/background.vert", "assets/background.frag");
+
+	ew::Shader shader("assets/vertexShader.vert", "assets/fragmentShader.frag");
+	ew::Shader characterShader("assets/characterShader.vert", "assets/characterShader.frag");
+																		
+	unsigned int quadVAO = createVAO(vertices, 4, indices, 6);
+
+	
+		unsigned int textureA = loadTexture("assets/bricks.jpg");
+		unsigned int textureB = loadTexture("assets/noiseTexture.png");
+		unsigned int character = loadTexture("assets/character.png");
+
+	glBindVertexArray(quadVAO);	
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+
+		//Place textureA in unit 0
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureA);
+		//Place textureB in unit 1
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, textureB);
+
+		//Must be using this shader when setting uniforms
+		shader.use();
+
+		//Make sampler2D _BrickTexture sample from unit 0
+		shader.setInt("_BrickTexture", 0);
+		//Make sampler2D _MarioTexture sample from unit 1
+		shader.setInt("_NoiseTexture", 1);
+		shader.setFloat("uTime", glfwGetTime());
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, NULL);
+		
+	
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, character);
+
+		characterShader.use();
+
+		characterShader.setInt("_CharacterTexture", 0);
+		characterShader.setFloat("uTime", glfwGetTime());
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, NULL);
+
+	
+		
+		//Render UI
+		{
+				
+		;
+
+			
+			ImGui_ImplGlfw_NewFrame();
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui::NewFrame();
+
+			ImGui::Begin("Settings");
+			ImGui::End();
+
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		}
+
+		glfwSwapBuffers(window);
+	}
+	printf("Shutting down...");
+}
+
+unsigned int createVAO(Vertex* vertexData, int numVertices, unsigned short* indicesData, int numIndices) {
+	unsigned int vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//Vertex Buffer Object 
+	unsigned int vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex)*numVertices, vertexData, GL_STATIC_DRAW);
+
+	//Element Buffer Object
+	unsigned int ebo;
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned short) * numIndices, indicesData, GL_STATIC_DRAW);
+
+	//Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)offsetof(Vertex,x));
+	glEnableVertexAttribArray(0);
+
+	//UV attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (const void*)(offsetof(Vertex, u)));
+	glEnableVertexAttribArray(1);
+
+	return vao;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
